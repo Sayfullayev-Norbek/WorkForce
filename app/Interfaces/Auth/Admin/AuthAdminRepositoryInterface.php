ccbee07a64060632a49aa9d1b48486c89d34071f
Sayfullayev Norbek <sayfullayev1572norbek@gmail.com>
2025-04-12 01:20:24 +0300

    delete

--- app/Interfaces/Auth/Admin/AuthAdminRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Interfaces\Auth\Admin;
-
-interface AuthAdminRepositoryInterface
-{
-    public function create(array $data);
-    public function login(array $credentials);
-}
--- app/Interfaces/Auth/Company/AuthCompanyRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Interfaces\Auth\Company;
-
-interface AuthCompanyRepositoryInterface
-{
-    public function create(array $data);
-    public function login(array $credentials);
-}
